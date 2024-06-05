6b7965b50923358050d5be18bd8d1c833853b735
kristin-min <kms@kristinkorea.com>
2024-06-05 16:25:03 +0900

    Model: (+)Question, Answer Model

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Question(Base):
+    # __tablename__: 모델에 의해 관리되는 Table의 이름
+    __tablename__ = "question"
+
+    # 디폴트 nullable=True
+    id = Column(Integer, primary_key=True)  # 고유 번호
+    title = Column(String, nullable=False)  # 질문 제목, 필수
+    content = Column(Text, nullable=False)  # 질문 내용, 필수
+    created = Column(DateTime, nullable=False)  # 작성일시, 필수
+
+
+class Answer(Base):
+    __tablename__ = "answer"
+
+    id = Column(Integer, primary_key=True)  # 고유 번호
+    content = Column(Text, nullable=False)  # 답변 내용
+    create_date = Column(DateTime, nullable=False)  # 작성일시, 필수
+    question_id = Column(Integer, ForeignKey("question.id"))  # 질문의 id
+    question = relationship(
+        "Question", backref="answer"
+    )  # Answer 모델에서 Question모델을 참조하기 위한 속성 relationship(참조할 속성, 역참조할 속성)
