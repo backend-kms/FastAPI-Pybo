29651eb14cc210c0d3c6b8401b237a3a11b494a3
kristin-min <kms@kristinkorea.com>
2024-06-10 13:04:25 +0900

    Add: /api/question/detail api

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -4,5 +4,10 @@ from models import Question
 
 
 def get_question_list(db: Session):
-    question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+    question_list = db.query(Question).order_by(Question.created.desc()).all()
     return question_list
+
+
+def get_question(db: Session, question_id: int):
+    question = db.query(Question).get(question_id)
+    return question
